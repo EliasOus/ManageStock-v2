d135e602adb088e3b54810a186f6de718bce8810
Ousameur Elias <ousameurelias@gmail.com>
2025-06-11 20:31:26 -0400

    modifer le composant inputForm

--- components/InputForm.jsx
+++ components/InputForm.jsx
@@ -5,102 +5,72 @@ import styles from "./InputForm.module.css";
 import { inputFormServer } from "@/actions/inputForm-action";
 
 export default function InputForm({ className, inputFields, onClose }) {
-  const [inputValues, setInputValues] = useState(
-    Object.fromEntries(inputFields.map((field) => [field.name, ""]))
-  );
-
-  const handleInputChange = (name, value) => {
-    setInputValues((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Loop through all input fields to check if any field is empty
-    for (const [key, value] of Object.entries(inputValues)) {
-      if (!value.trim()) {
-        alert(`Please fill in the ${key} field.`);
-        return; // Stop submission if a field is empty
-      }
-    }
-  };
-
   const [isInputVisible, setInputVisible] = useState(false);
-  const toggleInputForm = () => {
-    setInputVisible(true);
-  };
-  const handleCloseForm = () => {
-    setInputVisible(false);
+  const handleVisibleForm = () => {
+    !isInputVisible ? setInputVisible(true) : setInputVisible(false);
   };
 
   return (
     <>
-      {!isInputVisible && (
-        <div className={styles.boutons}>
-          <div onClick={toggleInputForm}>
-            <Button
-              texte={"Nouveau"}
-              active={true}
-              className={""}
-              type={"button"}
-            />
-          </div>
-
-          <div onClick={toggleInputForm}>
-            <Button
-              texte={"Modifier"}
-              active={true}
-              className={""}
-              type={"button"}
-            />
-          </div>
-
+      <div className={styles.boutons}>
+        <div onClick={handleVisibleForm}>
           <Button
-            texte={"Supprimer"}
+            texte={"Nouveau"}
             active={true}
             className={""}
             type={"button"}
           />
         </div>
-      )}
 
-      <form action={inputFormServer} className={`${styles.form} ${className}`}>
-        {inputFields.map((field, index) => (
-          <div key={index}>
-            {field.name === "description" ? (
-              // Render a textarea for the description input
-              <textarea
-                name={field.name}
-                value={inputValues[field.name]}
-                onChange={(e) => handleInputChange(field.name, e.target.value)}
-                placeholder={field.placeholder}
-                className={`${styles.input} ${styles.textarea}`} // Apply additional CSS for larger input
-              />
-            ) : (
-              // Render a normal input field
-              <input
-                type="text"
-                name={field.name}
-                value={inputValues[field.name]}
-                onChange={(e) => handleInputChange(field.name, e.target.value)}
-                placeholder={field.placeholder}
-                className={styles.input}
-              />
-            )}
-          </div>
-        ))}
-        <div className={styles.buttonContainer}>
-          <Button texte="Enregistrer" type="submit" active={true} />
-          {/* <button type="submit" onClick={onSubmit}>Enregistrer</button> */}
-          <div onClick={handleCloseForm}>
-          {/* <div onClick={onClose}> */}
-            <Button texte="Annuler" type="button" active={true} />
-          </div>
+        <div onClick={handleVisibleForm}>
+          <Button
+            texte={"Modifier"}
+            active={true}
+            className={""}
+            type={"button"}
+          />
         </div>
-      </form>
+
+        <Button
+          texte={"Supprimer"}
+          active={true}
+          className={""}
+          type={"button"}
+        />
+      </div>
+
+      {isInputVisible ? (
+        <form
+          action={inputFormServer}
+          className={`${styles.form} ${className}`}
+        >
+          {inputFields.map((field, index) => (
+            <div key={index}>
+              {field.name === "description" ? (
+                // Render a textarea for the description input
+                <textarea
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  className={`${styles.input} ${styles.textarea}`}
+                />
+              ) : (
+                <input
+                  type="text"
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  className={styles.input}
+                />
+              )}
+            </div>
+          ))}
+          <div className={styles.buttonContainer}>
+            <Button texte="Enregistrer" type="submit" active={true} />
+            <div onClick={handleVisibleForm}>
+              <Button texte="Annuler" type="button" active={true} />
+            </div>
+          </div>
+        </form>
+      ) : null}
     </>
   );
 }
