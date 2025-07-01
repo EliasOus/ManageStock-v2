94fcdf31985ddbfeed40787943d1e588b201f6a4
Ousameur Elias <ousameurelias@gmail.com>
2025-07-01 17:13:57 -0400

    corrige l'erreur suspense

--- app/(auth)/verification/page.jsx
+++ app/(auth)/verification/page.jsx
@@ -1,5 +1,6 @@
 import VerificationEmail from "@/components/VerificationEmail";
 import style from "./page.module.css"
+import { Suspense } from "react";
 
 export default function VerificationPage() {
 
